5f8573516a848e3e7683e1197e940e15b118d94d
data-cook <dtacook@gmail.com>
2016-12-02 12:45:49 +0100

    data

--- test_scripts/place_data.R
+++ test_scripts/place_data.R
@@ -11,3 +11,4 @@ place.2014 <- read_excel("./data/dodatki_agregat_2014.xlsx")
 
 place.2010.2014 <- rbind(place.2010, place.2011, place.2012, place.2013, place.2014)
 rm(place.2010, place.2011, place.2012, place.2013, place.2014)
+save.image(file = "./data/Place_2010_2014.RData")
