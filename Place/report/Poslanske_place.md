3eb6caa6fb42012dcb26ce06acb9c3e473712ee3
data-cook <dtacook@gmail.com>
2016-12-02 15:30:37 +0100

    summary

--- Place/report/Poslanske_place.md
+++ Place/report/Poslanske_place.md
@@ -7,3 +7,5 @@
 # Povprečne poslanske plače
 
 <img src="Poslanske_place_files/figure-html/povprečne plače-1.png" width="95%"  style="display: block; margin: auto;" />
+
+<img src="Poslanske_place_files/figure-html/01 povprečne plače-1.png" width="95%"  style="display: block; margin: auto;" />
--- Place/summary.R
+++ Place/summary.R
@@ -1,18 +1,25 @@
 library(tidyverse)
 library(forcats)
 library(tidyr)
+library(viridis)
 load(file = "./data/Place_2010_2014.RData")
 source("./functions/ggplot_tema.R")
 
 poslanci <- place.2010.2014 %>%
   filter(opis_delovnega_mesta_z370opis == "POSLANEC" & mesecev != 0 & placa > 0)
 
+poslanci <- poslanci %>%
+  mutate(`Povprečna bruto plača v €` = placa / mesecev)
+
+poslanci <- poslanci %>%
+  rename(Leto = leto_obdobja)
+
 
 povprecna.placa.leto <- poslanci %>%
   mutate(povprecna.placa = placa / mesecev) %>%
-  group_by(leto_obdobja) %>%
+  group_by(Leto) %>%
   summarize(povprecna.placa = round(mean(povprecna.placa, na.rm= T),0)) %>%
-  select(Leto = leto_obdobja, `Povprečna bruto plača v €` = povprecna.placa)
+  select(Leto, `Povprečna bruto plača v €` = povprecna.placa)
 
 g.povprecna.placa <- ggplot(povprecna.placa.leto, aes(x=Leto, y=`Povprečna bruto plača v €`)) +
   geom_bar(stat="identity") +
@@ -23,5 +30,27 @@ g.povprecna.placa <- ggplot(povprecna.placa.leto, aes(x=Leto, y=`Povprečna brut
   datacook_theme()
 
 ggsave("./Place/grafi/g.povprecna.placa.png", g.povprecna.placa, width = 306, height = 142, units = "mm", dpi = 300)
+
+
+
+g01.povprecna.placa <- ggplot(poslanci, aes(x=as.factor(Leto), y=`Povprečna bruto plača v €`)) +
+  geom_violin(aes(fill = as.factor(Leto))) +
+  geom_jitter(height = 0,size = 1.5 ) +
+  scale_fill_viridis(discrete = T, begin = 0.3, end = 0.6)+
+  facet_wrap(~Leto, scales = "free_x", nrow = 1) +
+  geom_label(data = povprecna.placa.leto, aes(x = as.factor(Leto),
+                                              label = paste("Povprečna plača\n",`Povprečna bruto plača v €`)))+
+  labs(title = "Povprečna mesečna plača poslanca po letih",
+       subtitle="",
+       caption = "Vir podatkov:\nhttps://podcrto.si/podatkovne-zbirke/\nhttp://resources.podcrto.si/dodatkizaobjavo.rar",
+       x = "Leto") +
+  datacook_theme() +
+  theme(legend.title = element_blank(),
+        legend.position = "bottom",
+        legend.direction = "horizontal")
+
+ggsave("./Place/grafi/g01.povprecna.placa.png", g01.povprecna.placa, width = 306, height = 142, units = "mm", dpi = 300)
+
+
 
 save.image(file = "./Place/data/Poslanske_place.RData")
