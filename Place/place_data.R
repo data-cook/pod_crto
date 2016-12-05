b9b5e0af2a50b2c0260e8c177556630853f7f8c6
data-cook <dtacook@gmail.com>
2016-12-05 15:55:22 +0100

    summary

--- Place/place_data.R
+++ Place/place_data.R
@@ -11,4 +11,32 @@ place.2014 <- read_excel("./data/dodatki_agregat_2014.xlsx")
 
 place.2010.2014 <- rbind(place.2010, place.2011, place.2012, place.2013, place.2014)
 rm(place.2010, place.2011, place.2012, place.2013, place.2014)
+
+place.2010.2014 <- place.2010.2014 %>%
+  rename(leto = leto_obdobja)
+
+place.2010.2014$skupina_dm <- as.factor(place.2010.2014$skupina_dm)
+place.2010.2014 <- place.2010.2014 %>%
+  rename(Skupina = skupina_dm)
+
+place.2010.2014$Skupina <- as.character(place.2010.2014$Skupina)
+place.2010.2014$Skupina[place.2010.2014$Skupina == "A"] <- "Funkcionarji (A)"
+place.2010.2014$Skupina[place.2010.2014$Skupina == "B"] <- "Vodilni uslužbenci (B)"
+place.2010.2014$Skupina <- factor(place.2010.2014$Skupina)
+
+
+place.2010.2014.long <- gather(data = place.2010.2014, key = "placilo", value = "vrednost", placa, polozajni_dodatek:dodatek_poracun_c900bruto)
+place.2010.2014.long$placilo <- as.factor(place.2010.2014.long$placilo)
+
+place.2010.2014.long <- place.2010.2014.long %>%
+  mutate(`Povprečna vrednost` = vrednost/mesecev)
+
+place.2010.2014.long$`Povprečna vrednost` <- ifelse(is.na(place.2010.2014.long$`Povprečna vrednost`),
+                                                    place.2010.2014.long$vrednost,
+                                                    place.2010.2014.long$`Povprečna vrednost`)
+place.2010.2014.long$`Povprečna vrednost` <- ifelse(place.2010.2014.long$mesecev == 0,
+                                                    place.2010.2014.long$vrednost,
+                                                    place.2010.2014.long$`Povprečna vrednost`)
+
+
 save.image(file = "./data/Place_2010_2014.RData")
--- Place/summary.R
+++ Place/summary.R
@@ -5,46 +5,29 @@ library(viridis)
 load(file = "./data/Place_2010_2014.RData")
 source("./functions/ggplot_tema.R")
 
+povprecna.izplacila <- place.2010.2014.long %>%
+  group_by(placilo, leto) %>%
+  summarise(`Povprečna vrednost` = round(mean(`Povprečna vrednost`, na.rm=T),2)) %>%
+  ungroup()
 
-place.2010.2014$skupina_dm <- as.factor(place.2010.2014$skupina_dm)
-place.2010.2014 <- place.2010.2014 %>%
-  rename(Skupina = skupina_dm)
+poslanci <- place.2010.2014.long %>%
+  filter(opis_delovnega_mesta_z370opis == "POSLANEC" & placilo == "placa" & vrednost > 0)
 
-place.2010.2014 <- place.2010.2014 %>%
-  mutate(`Povprečna bruto plača v €` = placa / mesecev)
+poslanci$leto <- as.factor(poslanci$leto)
 
-place.2010.2014 <- place.2010.2014 %>%
-  rename(Leto = leto_obdobja)
+poslanci.povprecna.placa.leto <- poslanci %>%
+  group_by(leto) %>%
+  summarize(povprecna.placa = round(mean(`Povprečna vrednost`, na.rm= T),0)) %>%
+  select(leto, `Povprečna vrednost` = povprecna.placa) %>%
+  ungroup()
 
-place.2010.2014$Leto <- as.factor(place.2010.2014$Leto)
-
-
-
-poslanci <- place.2010.2014 %>%
-  filter(opis_delovnega_mesta_z370opis == "POSLANEC" & mesecev != 0 & placa > 0)
-
-poslanci <- poslanci %>%
-  mutate(`Povprečna bruto plača v €` = placa / mesecev)
-
-poslanci <- poslanci %>%
-  rename(Leto = leto_obdobja)
-
-poslanci$Leto <- as.factor(poslanci$Leto)
-poslanci$skupina_dm <- as.factor(poslanci$skupina_dm)
-poslanci <- poslanci %>%
-  rename(Skupina = skupina_dm)
-
-povprecna.placa.leto <- poslanci %>%
-  mutate(povprecna.placa = placa / mesecev) %>%
-  group_by(Leto) %>%
-  summarize(povprecna.placa = round(mean(povprecna.placa, na.rm= T),0)) %>%
-  select(Leto, `Povprečna bruto plača v €` = povprecna.placa)
-
-g.povprecna.placa <- ggplot(povprecna.placa.leto, aes(x=Leto, y=`Povprečna bruto plača v €`)) +
+g.povprecna.placa <- ggplot(poslanci.povprecna.placa.leto, aes(x=leto, y=`Povprečna vrednost`)) +
   geom_bar(stat="identity") +
-  geom_label(aes(label=paste(`Povprečna bruto plača v €`, "€"))) +
+  geom_label(aes(label=paste(`Povprečna vrednost`, "€"))) +
   labs(title = "Povprečna mesečna plača poslanca po letih",
        subtitle="",
+       x = "Leto",
+       y="Povprečna bruto plača v €",
        caption = "Vir podatkov:\nhttps://podcrto.si/podatkovne-zbirke/\nhttp://resources.podcrto.si/dodatkizaobjavo.rar") +
   datacook_theme()
 
@@ -52,17 +35,18 @@ ggsave("./Place/grafi/g.povprecna.placa.png", g.povprecna.placa, width = 306, he
 
 
 
-g01.povprecna.placa <- ggplot(poslanci, aes(x=Leto, y=`Povprečna bruto plača v €`)) +
-  geom_violin(aes(fill = Leto)) +
+g01.povprecna.placa <- ggplot(poslanci, aes(x=leto, y=`Povprečna vrednost`)) +
+  geom_violin(aes(fill = leto)) +
   geom_jitter(height = 0,size = 1.5 ) +
   scale_fill_viridis(discrete = T, begin = 0.3, end = 0.6)+
-  facet_wrap(~Leto, scales = "free_x", nrow = 1) +
-  geom_label(data = povprecna.placa.leto, aes(x = Leto,
-                                              label = paste("Povprečna plača\n",`Povprečna bruto plača v €`)))+
+  facet_wrap(~leto, scales = "free", nrow = 1) +
+  geom_label(data = poslanci.povprecna.placa.leto, aes(x = leto,
+                                              label = paste("Povprečna plača\n",`Povprečna vrednost`)))+
   labs(title = "Povprečna mesečna plača poslanca po letih",
        subtitle="",
-       caption = "Vir podatkov:\nhttps://podcrto.si/podatkovne-zbirke/\nhttp://resources.podcrto.si/dodatkizaobjavo.rar",
-       x = "Leto") +
+       x = "Leto",
+       y="Povprečna bruto plača v €",
+       caption = "Vir podatkov:\nhttps://podcrto.si/podatkovne-zbirke/\nhttp://resources.podcrto.si/dodatkizaobjavo.rar") +
   datacook_theme() +
   theme(legend.title = element_blank(),
         legend.position = "bottom",
@@ -70,7 +54,7 @@ g01.povprecna.placa <- ggplot(poslanci, aes(x=Leto, y=`Povprečna bruto plača v
 
 ggsave("./Place/grafi/g01.povprecna.placa.png", g01.povprecna.placa, width = 306, height = 142, units = "mm", dpi = 300)
 
-
+#############################
 
 skupina.delovega.mesta <- place.2010.2014 %>%
   mutate(povprecna.placa = placa / mesecev) %>%
@@ -78,10 +62,7 @@ skupina.delovega.mesta <- place.2010.2014 %>%
   summarize(povprecna.placa = round(mean(povprecna.placa, na.rm= T),0)) %>%
   select(Skupina, `Povprečna bruto plača v €` = povprecna.placa)
 
-skupina.delovega.mesta$Skupina <- as.character(skupina.delovega.mesta$Skupina)
-skupina.delovega.mesta$Skupina[skupina.delovega.mesta$Skupina == "A"] <- "Funkcionarji (A)"
-skupina.delovega.mesta$Skupina[skupina.delovega.mesta$Skupina == "B"] <- "Vodilni uslužbenci (B)"
-skupina.delovega.mesta$Skupina <- factor(skupina.delovega.mesta$Skupina)
+
 
 g.povprecna.placa.skupina <- ggplot(skupina.delovega.mesta, aes(x=Skupina, y=`Povprečna bruto plača v €`)) +
   geom_bar(stat="identity") +
@@ -92,6 +73,22 @@ g.povprecna.placa.skupina <- ggplot(skupina.delovega.mesta, aes(x=Skupina, y=`Po
   datacook_theme()
 
 ggsave("./Place/grafi/g.povprecna.placa.skupina.png", g.povprecna.placa.skupina, width = 306, height = 142, units = "mm", dpi = 300)
+
+
+
+## povprečna izplačila dodatkov
+izplacila <- povprecna.izplacila %>%
+  filter(placilo != "placa") %>%
+  select(placilo, leto, vrednost = `Povprečna vrednost`)
+
+
+izplacila <- as.data.frame(izplacila)
+
+
+ggplot(data = izplacila, aes(x=leto, y=vrednost)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  facet_wrap(~placilo,scales = "free")
 
 
 save.image(file = "./Place/data/Poslanske_place.RData")
