c793cb7e2468bda763cc309975fec5278f7ebfe8
data-cook <dtacook@gmail.com>
2016-12-07 11:34:13 +0100

    summary

--- Place/report/Poslanske_place.md
+++ Place/report/Poslanske_place.md
@@ -4,15 +4,20 @@
 
 
 
-# Povprečne plače vodilnih uslužbencev v javnem sektorju v 2010 – 2014
+#
+
+## Po skupini delovnega mesta
+
+<img src="Poslanske_place_files/figure-html/Po skupini delovnega mesta-1.png" width="95%"  style="display: block; margin: auto;" />
 
 ## Poslanci
+<img src="Poslanske_place_files/figure-html/Poslanci-1.png" width="95%"  style="display: block; margin: auto;" />
 
-<img src="Poslanske_place_files/figure-html/povprecne place-1.png" width="95%"  style="display: block; margin: auto;" />
 
 ### Malo drugače
-<img src="Poslanske_place_files/figure-html/01 povprecne place-1.png" width="95%"  style="display: block; margin: auto;" />
+<img src="Poslanske_place_files/figure-html/Malo drugače-1.png" width="95%"  style="display: block; margin: auto;" />
+
+## Dodatki
+<img src="Poslanske_place_files/figure-html/Dodatki-1.png" width="95%"  style="display: block; margin: auto;" />
 
 
-## Po skupinah delovnega mesta
-<img src="Poslanske_place_files/figure-html/skupina delovnega mesta-1.png" width="95%"  style="display: block; margin: auto;" />
--- Place/summary.R
+++ Place/summary.R
@@ -2,11 +2,14 @@ library(tidyverse)
 library(forcats)
 library(tidyr)
 library(viridis)
+library(plotly)
 #library(ggiraphExtra)
 #library(ggiraph)
 load(file = "./data/Place_2010_2014.RData")
 source("./functions/ggplot_tema.R")
 
+
+# Poslanci ------------------------------------------------------------------------------------
 poslanci <- place.2010.2014.long %>%
   filter(opis_delovnega_mesta_z370opis == "POSLANEC" & placilo == "placa" & vrednost > 0)
 
@@ -18,7 +21,7 @@ poslanci.povprecna.placa.leto <- poslanci %>%
   select(leto, `Povprečna vrednost` = povprecna.placa) %>%
   ungroup()
 
-g.povprecna.placa <- ggplot(poslanci.povprecna.placa.leto, aes(x=leto, y=`Povprečna vrednost`)) +
+g.povprecna.placa.poslanci <- ggplot(poslanci.povprecna.placa.leto, aes(x=leto, y=`Povprečna vrednost`)) +
   geom_bar(stat="identity") +
   geom_label(aes(label=paste(`Povprečna vrednost`, "€"))) +
   labs(title = "Povprečna mesečna plača poslanca po letih",
@@ -28,10 +31,10 @@ g.povprecna.placa <- ggplot(poslanci.povprecna.placa.leto, aes(x=leto, y=`Povpre
        caption = "Vir podatkov:\nhttps://podcrto.si/podatkovne-zbirke/\nhttp://resources.podcrto.si/dodatkizaobjavo.rar") +
   datacook_theme()
 
-ggsave("./Place/grafi/g.povprecna.placa.png", g.povprecna.placa, width = 306, height = 142, units = "mm", dpi = 300)
+ggsave("./Place/grafi/g.povprecna.placa.poslanci.png", g.povprecna.placa.poslanci, width = 306, height = 142, units = "mm", dpi = 300)
 
 
-g01.povprecna.placa <- ggplot(poslanci, aes(x=leto, y=`Povprečna vrednost`)) +
+g01.povprecna.placa.poslanci <- ggplot(poslanci, aes(x=leto, y=`Povprečna vrednost`)) +
   geom_violin(aes(fill = leto)) +
   geom_jitter(height = 0,size = 1.5 ) +
   scale_fill_viridis(discrete = T, begin = 0.3, end = 0.6)+
@@ -48,70 +51,65 @@ g01.povprecna.placa <- ggplot(poslanci, aes(x=leto, y=`Povprečna vrednost`)) +
         legend.position = "bottom",
         legend.direction = "horizontal")
 
-ggsave("./Place/grafi/g01.povprecna.placa.png", g01.povprecna.placa, width = 306, height = 142, units = "mm", dpi = 300)
 
+ggsave("./Place/grafi/g01.povprecna.placa.poslanci.png", g01.povprecna.placa.poslanci, width = 306, height = 142, units = "mm", dpi = 300)
+# Poslanci ------------------------------------------------------------------------------------
+
+
+# Dodatki -------------------------------------------------------------------------------------
 povprecna.izplacila.dodatkov <- place.2010.2014.long %>%
   filter(placilo != "placa" & vrednost !=0) %>%
   group_by(placilo, leto) %>%
   summarise(`Povprečna vrednost` = round(mean(`Povprečna vrednost`, na.rm=T),2)) %>%
   ungroup()
 
+povprecna.izplacila.dodatkov$leto <- as.factor(povprecna.izplacila.dodatkov$leto)
 
 g02.povprecna.izplacila.dodatkov <- povprecna.izplacila.dodatkov %>%
-  filter(leto == "2010") %>%
-  ggplot(., aes(x=placilo, y=`Povprečna vrednost`)) +
+  #filter(leto == "2010") %>%
+  ggplot(., aes(x=leto, y=`Povprečna vrednost`, ymax=`Povprečna vrednost`+50)) + 
   geom_bar(stat="identity") +
-  facet_wrap(~leto, scales = "free", nrow = 1) +
-  geom_label(aes(label=`Povprečna vrednost`)) +
-  labs(title = "",
+  facet_wrap(~placilo, scales = "free_y", ncol = 6) +
+  geom_text(aes(label=`Povprečna vrednost`), vjust=-0.5, angle = 0) +
+  labs(title = "Povprečna bruto izplačila dodatkov",
        subtitle="",
        x = "",
-       y="",
+       y="Povprečno bruto izplačilo v €",
        caption = "Vir podatkov:\nhttps://podcrto.si/podatkovne-zbirke/\nhttp://resources.podcrto.si/dodatkizaobjavo.rar") +
   datacook_theme() +
   theme(legend.title = element_blank(),
         legend.position = "bottom",
         legend.direction = "horizontal",
-        axis.text.x = element_text(size = 10, angle = 45, hjust = 1))
+        axis.text.x = element_text(size = 10, angle = 0),
+        strip.text = element_text(size = 8))
 
 ggsave("./Place/grafi/g02.povprecna.izplacila.dodatkov.png", g02.povprecna.izplacila.dodatkov, width = 306, height = 142, units = "mm", dpi = 300)
+# Dodatki -------------------------------------------------------------------------------------
 
 
+# Skupina delovnega mesta ---------------------------------------------------------------------
 
+skupina.delovega.mesta.placa <- place.2010.2014.long %>%
+  filter(placilo == "placa" & vrednost !=0) %>%
+  group_by(leto, Skupina) %>%
+  summarise(`Povprečna vrednost` = round(mean(`Povprečna vrednost`, na.rm=T),2)) %>%
+  ungroup()
 
-skupina.delovega.mesta <- place.2010.2014 %>%
-  mutate(povprecna.placa = placa / mesecev) %>%
-  group_by(Skupina) %>%
-  summarize(povprecna.placa = round(mean(povprecna.placa, na.rm= T),0)) %>%
-  select(Skupina, `Povprečna bruto plača v €` = povprecna.placa)
+skupina.delovega.mesta.placa$leto <- as.factor(skupina.delovega.mesta.placa$leto)
 
-
-
-g.povprecna.placa.skupina <- ggplot(skupina.delovega.mesta, aes(x=Skupina, y=`Povprečna bruto plača v €`)) +
+g.povprecna.placa.skupina <- ggplot(skupina.delovega.mesta.placa, aes(x=Skupina, y=`Povprečna vrednost`)) +
   geom_bar(stat="identity") +
-  geom_label(aes(label=paste(`Povprečna bruto plača v €`, "€"))) +
-  labs(title = "Povprečna mesečna plača po skupinah delovnega mesta",
+  geom_label(aes(label=paste(`Povprečna vrednost`, "€"))) +
+  facet_wrap(~leto, nrow = 1) +
+  labs(title = "Povprečna bruto mesečna plača po skupinah delovnega mesta",
        subtitle="",
+       x="Skupina delovnega mesta",
+       y="Povprečno bruto plača v €",
        caption = "Vir podatkov:\nhttps://podcrto.si/podatkovne-zbirke/\nhttp://resources.podcrto.si/dodatkizaobjavo.rar") +
   datacook_theme()
 
 ggsave("./Place/grafi/g.povprecna.placa.skupina.png", g.povprecna.placa.skupina, width = 306, height = 142, units = "mm", dpi = 300)
-
-
-
-## povprečna izplačila dodatkov
-izplacila <- povprecna.izplacila %>%
-  filter(placilo != "placa") %>%
-  select(placilo, leto, vrednost = `Povprečna vrednost`)
-
-
-izplacila <- as.data.frame(izplacila)
-
-
-ggplot(data = izplacila, aes(x=leto, y=vrednost)) +
-  geom_bar(stat = "identity") +
-  coord_flip() +
-  facet_wrap(~placilo,scales = "free")
+# Skupina delovnega mesta ---------------------------------------------------------------------
 
 
 save.image(file = "./Place/data/Poslanske_place.RData")
